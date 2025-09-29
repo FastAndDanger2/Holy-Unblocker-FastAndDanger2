aeb020a032a9e1e05598ed3199e770d3b3099756
Ojas Sarada <144619159+FastAndDanger2@users.noreply.github.com>
2025-09-28 23:18:42 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,3 +1,9 @@
 (async () => {
   await import('./src/server.mjs');
 })();
+
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
+app.listen(port, host, () => {
+  console.log(`Listening on port ${port}`);
+});
