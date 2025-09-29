235d873145b3b9b2760514c4481938e2e9314d7b
Ojas Sarada <ojassarada@Mac.attlocal.net>
2025-09-28 23:48:45 -0500

    Edited backend.js to adjust server start

--- backend.js
+++ backend.js
@@ -1,9 +1,2 @@
-(async () => {
-  await import('./src/server.mjs');
-})();
+import './src/server.mjs';
 
-const port = process.env.PORT || 3000;
-const host = '0.0.0.0';
-app.listen(port, host, () => {
-  console.log(`Listening on port ${port}`);
-});
